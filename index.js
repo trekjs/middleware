d8780976892a4a6412d9d4d74046d840fb657b0a
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-09-15 23:43:52 +0800

    should not enter on infinite loop

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
+'use strict'
+
 module.exports = class Middleware extends Array {
 
-  next (i = 0, context, nextFunc) {
-    const fn = this[i] || nextFunc
-    let called = false
+  next (i = 0, context, last, done = false, called = false, fn) {
+    done = i > this.length
+    if (done) return { done }
+
+    fn = this[i] || last
 
     return fn && fn(context, () => {
-      if (called) {
-        throw new Error('next() called multiple times')
-      }
+      if (called) throw new Error('next() called multiple times')
       called = true
-      // If you really want to use in excess of 5k middleware, ex:
-      // return i > 5120 ? Promise.resolve().then(() => this.next(i, context, nextFunc)) : Promise.resolve(this.next(i, context, nextFunc))
-      return Promise.resolve(this.next(i + 1, context, nextFunc))
+      return Promise.resolve(this.next(i + 1, context, last))
     })
   }
 
-  compose (context, nextFunc) {
+  compose (context, last) {
     try {
-      return Promise.resolve(this.next(0, context, nextFunc))
+      return Promise.resolve(this.next(0, context, last))
     } catch (err) {
       return Promise.reject(err)
     }
